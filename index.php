d75c87148332390a6aebeda832b8addf571086ed
Will Gallia <wgallia@gmail.com>
2015-02-25 14:13:29 +0000

    moved static to own root directory

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
+error_reporting(E_ERROR);
 
 if(isset($_GET['_escaped_fragment'])) {
     $frag = urldecode($_GET['_escaped_fragment']);
     if($frag == '') {
-        exit(file_get_contents('snaps/static/crawl.html'));
+        exit(file_get_contents('static/crawl.html'));
     }
     else {
-        $file = file_get_contents('snaps/'.$frag.'.html');
+        $file = file_get_contents(''.$frag.'.html');
         if($file) exit($file);
-        else exit(file_get_contents('snaps/static/page_not_found.html'));
+        else exit(file_get_contents('static/page_not_found.html'));
     }
 }
 else { //person not machine
-    exit(file_get_contents('snaps/static/index.html'));
+    exit(file_get_contents('static/index.html'));
 }
 
 ?>
